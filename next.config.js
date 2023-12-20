9876ada95eabd0fed9a61f2f8d9d48f4852eb951
Andras Hejj <andras@nobank.one>
2023-12-20 18:55:58 +0100

    feat: custom font and tailwind

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.tsx",
+  defaultShowCopyCode: true,
+  latex: true,
+  flexsearch: {
+    codeblock: false,
+  },
+  readingTime: true,
 });
 
 module.exports = withNextra({
+  reactStrictMode: true,
+  cleanDistDir: true,
   i18n: {
     locales: ["en"], // TODO: Add de when needed
     defaultLocale: "en",
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,5 +10,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [],
 };
