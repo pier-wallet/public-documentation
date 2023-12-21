d1d03f51ef96755351769722e9ba8709424a2b23
Andras Hejj <andras@nobank.one>
2023-12-21 17:09:21 +0100

    feat: font stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./pages/**/*.{js,jsx,ts,tsx,md,mdx}",
@@ -8,6 +9,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx,md,mdx}",
   ],
   theme: {
+    fontFamily: {
+      // sans: ["var(--font-gt-flexa)"],
+      // mono: ["var(--font-gt-flexa)"],
+      // serif: ["var(--font-gt-flexa)"],
+      sans: ["var(--font-inconsolata)", "sans-serif"],
+      mono: ["var(--font-inconsolata)", "monospace"],
+      serif: ["var(--font-inconsolata)", "serif"],
+    },
     extend: {},
   },
   plugins: [],
